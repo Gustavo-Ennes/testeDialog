15f756293f49e986345cf697b68d8a1ec0ab5ccc
Gustavo-Ennes <gustavo.ennes@3778.care>
2024-08-15 15:16:22 -0300

    relational database

--- packages/api-node/config/db.js
+++ packages/api-node/config/db.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./database.sqlite",
+// });
+const sequelize = new Sequelize("database", "sa", "1Senha!.", {
+    host: "localhost",
+    dialect: "postgres",
 });
 
 module.exports = { sequelize };
